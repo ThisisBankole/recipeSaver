273f4e19aacd658cb97c32e264baca4c381b0be0
Bankole Jamgbadi <bjamgbadi@gmail.com>
2022-04-20 09:19:07 +0100

    Initial Commit

---
+++ recipeSaver/recipeSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipeSaverApp.swift
+//  recipeSaver
+//
+//  Created by user on 2022-04-20.
+//
+
+import SwiftUI
+
+@main
+struct recipeSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
